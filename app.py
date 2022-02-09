82ce13255adbd6a135b117c466bf933fe7369990
Aditya Pratap Singh <cybercreed010@gmail.com>
2022-02-09 23:52:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1> Hello  World !! Welcome To Cyber Creed </h1>"
+    return "<h1> Hello  World !! Welcome To Cyber Creed.... </h1>"
 
 
 # main driver function
